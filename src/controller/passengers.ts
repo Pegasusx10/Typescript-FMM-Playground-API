3da26adf0e0935040aff9e65b30273cca37912fb
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2023-01-17 17:59:31 +0530

    Update passengers.ts

--- src/controller/passengers.ts
+++ src/controller/passengers.ts
@@ -1,84 +1,93 @@
 import { RequestHandler } from "express";
-import flights from "../models/flights";
-const queryCondition = require('../utils/passengerValidation')
+import Flights, { flightsModel } from "../models/flights";
+const queryCondition = require('../utils/flightValidation')
 
-import passengers, { passengersModel } from "../models/passengers";
 
-// Create a single Passenger
-export const createPassengers: RequestHandler = async (req, res) => {
-  try {
-  const newPassenger = new passengers({
-  firstName: req.body.firstName,
-  lastName: req.body.lastName,
-  age: req.body.age,
-  passportNo: req.body.passportNo,
-  country: req.body.country
+// Create a flight
+export const createFlights: RequestHandler = async (req, res) => {
+  const newFlight = new Flights({
+    flightNumber: req.body.flightNumber,
+    tailNumber: req.body.tailNumber,
+    origin: req.body.origin,
+    destination: req.body.destination,
+    iropStatus: req.body.iropStatus,
+    totalSeats: req.body.totalSeats,
+    passengers: req.body.passengers,
+    hasBusinessClass: req.body.hasBusinessClass,
+    delay: req.body.delay,
   });
-  const freshPassenger = await newPassenger.save();
-  res.status(201).json(freshPassenger);
-  } catch(err) {
-  res.status(404).json("The Passenger you're looking for does not exist!");
+  try {
+    const freshFlights = await newFlight.save() as flightsModel;
+    res.status(201).send(freshFlights);
+  } catch (err) {
+    res.status(404).send({ message: `The Flight you're looking for does not exist!` });
   }
 };
 
-// Get all Passengers
-export const getPassengers: RequestHandler = async (req, res) => {
+// Get all flights
+export const getFlights: RequestHandler = async (req, res) => {
   try {
     let pageSize = req.query.pageSize;
     let pageNumber = req.query.pageNumber;
     if (typeof pageSize !== 'string') {
       pageSize = "0";
-    }
+    };
     if (typeof pageNumber !== 'string') {
       pageNumber = "1";
-    }
+    };
     const pageSizeValue = parseInt(pageSize, 10) || 0;
     const pageNumberValue = parseInt(pageNumber, 10) || 1;
     const queries = queryCondition(req.query)
-    const Passengers = await passengers
+    const flight = Flights
       .find(queries)
       .limit(pageSizeValue)
       .skip((pageNumberValue - 1) * pageSizeValue)
-      res.status(200).json(Passengers)
+      .populate('passengers')
+      .populate('delays')
+      res.status(200).send(Flight)
     } catch (err) {
-      res.status(404).json({ message: "The Delay you are looking for does not exist!" })
+      res.status(404).send({ message: "The Flight you are looking for does not exist!" });
     }
-  };
-  
+  };  
 
-// get a single Passenger
-export const getPassenger: RequestHandler = async (req, res) => {
+// Get a single flight 
+export const getFlight: RequestHandler = async (req, res) => {
   try{
-    const passengerInfo = await flights.findById(req.params.id)
-    // .populate('enrolledCourses')
-     res.send(passengerInfo)
-    } catch (err) {
-      res.status(404).json(`The Passenger you're looking for does not exist!`)
-    }
-};
-
-// Update a single Passenger
-export const updatePassengers: RequestHandler = async (req, res) => {
-  try {
-    const { id } = req.params;
-    var Passengers = await passengers.findByIdAndUpdate(id, req.body, { new: true });
-    return res
-      .status(200)
-      .json({ message: "delays updated successfully!", data: Passengers });
-  } catch (error: any) {
-    return res.status(500).json(`The Passenger you are looking for does not exsist!`);
+    const flight = Flights.findById(req.params.id)
+    .populate('passengers')
+    .populate('delays') as flightsModel;
+    res.status(200).send(flight);
+  } catch (err) {
+    res.status(404).send({ message: `The Flight you're looking for does not exist!` });
   }
 };
 
-// Delete a single Passenger
-export const deletePassengers: RequestHandler = async (req, res) => {
+// endpoint for cancelled Flights
+export const cancelledFlights: RequestHandler = async (req, res) => {
   try {
-    const { id } = req.params;
-    var isDeleted = await passengers.findByIdAndDelete(id);
-    if (!isDeleted) throw new Error("Failed to delete todo");
-    return res.status(200).json({ message: "Todo deleted successfully!" });
-  } catch (error: any) {
-    return res.status(500).json(`The Passenger you are looking for does not exsist!`);
+      const flights = Flights.find({ iropStatus: 'CX' });
+      res.json(Flights);
+  } catch (err) {
+      res.status(500).send(`The endpoint URL does not exsist!`);
   }
 };
 
+// Update flight by ID
+export const updateFlights: RequestHandler = async (req, res) => {
+  try{
+    const updatedFlight = Flights.findByIdAndUpdate(req.params.id, req.body, {new: true}) as flightsModel;
+    res.status(200).send(updatedFlight);
+  } catch (err) {
+    res.status(500).send({ message: `An error occurred while updating the record` });
+  }
+};
+
+// Delete a single Flight
+export const deleteFlights: RequestHandler = async (req, res) => {
+  try {
+    const flightId = Flights.findByIdAndDelete(req.params.id)
+    res.send(`The Flight has been deleted from database successfully!`)
+  } catch (err) {
+    res.status(404).send(`The Flight you're looking for does not exist!`)
+  }
+};
