2fa7560084c6a8dc0611a1affb3fd47c7a8bab84
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2023-01-23 11:43:17 +0530

    Update passengers.ts

--- src/controller/passengers.ts
+++ src/controller/passengers.ts
@@ -1,79 +1,78 @@
-import { RequestHandler } from "express";
-import Passengers from "../models/passengers";
-const queryCondition = require('../utils/queryLogic')
+import { RequestHandler, Request, Response } from "express";
+import Passengers, { passengersModel } from "../models/passengers";
+import queryCondition from '../utils/query';
+import { handlePassengerError, handlePassengerNotFound } from "../middleware/errorHandler";
 
-
-// Create a Passenger
-export const createPassengers: RequestHandler = async (req, res) => {
-  const newPassengers = new Passengers({
-    firstName:  req.body.firstName,
-    lastName: req.body.lastName,
-    age: req.body.age,
-    passportNo: req.body.passportNo,
-    country: req.body.country,
+// Create a single Passenger
+export const createPassengers: RequestHandler = async (req: Request, res: Response) => {
+  const newPassenger = new Passengers({
+  firstName: req.body.firstName,
+  lastName: req.body.lastName,
+  age: req.body.age,
+  passportNo: req.body.passportNo,
+  country: req.body.country
   });
   try {
-    const freshPassengers = await newPassengers.save();
-    res.status(201).send(freshPassengers);
-  } catch (err) {
-    res.status(404).send({ message: `The Flight you're looking for does not exist!` });
+  const freshPassenger = await newPassenger.save();
+  res.status(201).json(freshPassenger);
+  } catch(err) {
+    handlePassengerNotFound
   }
 };
 
 // Get all Passengers
-export const getPassengers: RequestHandler = async (req, res) => {
+export const getPassengers: RequestHandler = async (req: Request, res: Response) => {
   try {
     let pageSize = req.query.pageSize;
     let pageNumber = req.query.pageNumber;
     if (typeof pageSize !== 'string') {
       pageSize = "0";
-    };
+    }
     if (typeof pageNumber !== 'string') {
       pageNumber = "1";
-    };
+    }
     const pageSizeValue = parseInt(pageSize, 10) || 0;
     const pageNumberValue = parseInt(pageNumber, 10) || 1;
     const queries = queryCondition(req.query)
-    const passenger = Passengers
+    const allPassengers =  await Passengers
       .find(queries)
       .limit(pageSizeValue)
       .skip((pageNumberValue - 1) * pageSizeValue)
-      // .populate('passengers')
-      // .populate('delays')
-      res.status(200).send(passenger)
+      res.status(200).send(allPassengers)
     } catch (err) {
-      res.status(404).send({ message: "The Flight you are looking for does not exist!" });
+      handlePassengerNotFound
     }
-  };  
-
-// Get a single Passenger
-export const getPassenger: RequestHandler = async (req, res) => {
-  try{
-    const onePassenger = Passengers.findById(req.params.id)
-    // .populate('passengers')
-    // .populate('delays');
-    res.status(200).send(onePassenger);
-  } catch (err) {
-    res.status(404).send({ message: `The Flight you're looking for does not exist!` });
-  }
-};
-
-// Update flight by ID
-export const updatePassengers: RequestHandler = async (req, res) => {
-  try{
-    const updatedPassenger = Passengers.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    res.status(200).send(updatedPassenger);
-  } catch (err) {
-    res.status(500).send({ message: `An error occurred while updating the record` });
-  }
-};
-
-// Delete a single Flight
-export const deletePassengers: RequestHandler = async (req, res) => {
+  };
+  
+// Get a single passenger 
+export const getPassenger: RequestHandler = async (req: Request, res: Response) => {
   try {
-    const flightId = Passengers.findByIdAndDelete(req.params.id)
-    res.send(`The Flight has been deleted from database successfully!`)
+    const passengerInfo = await Passengers.findById(req.params.id);
+    res.status(200).send(passengerInfo);
   } catch (err) {
-    res.status(404).send(`The Flight you're looking for does not exist!`)
+    handlePassengerNotFound;
+  }
+};
+
+// Update a single passenger
+export const updatePassengers: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const updatedPassenger = await Passengers.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).send({ message: "Passenger updated successfully!", data: updatedPassenger });
+  } catch (err) {
+    handlePassengerNotFound
+  }
+};
+
+// Delete a single Passenger
+export const deletePassengers: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    var isDeleted = await Passengers.findByIdAndDelete(id);
+    if (!isDeleted) throw new Error("Failed to delete passengers");
+    return res.status(200).send({ message: "passenger deleted successfully!" });
+  } catch (error: any) {
+    handlePassengerError
   }
 };
