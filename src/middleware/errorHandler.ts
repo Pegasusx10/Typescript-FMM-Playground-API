59f2ae5b6efd6716b634632b7b9f4f65842ef704
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2023-01-23 11:44:20 +0530

    Updated error handler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,15 +1,38 @@
-import { Request, Response, NextFunction } from 'express';
-// import HttpException from '../utils/exceptions/http.exception';
+import { Response } from 'express-serve-static-core';
 
-function errorMiddleware(
-    err: HttpException,
-    req: Request,
-    res: Response,
-    next: NextFunction
-): void {
-    const status = err.status || 500;
-    const message = err.message || 'Something wend wrong';
-    res.status(status).json({ status, message });
+
+export const handleError = (res: Response, err: any) => {
+    res.status(401).json({ message: `Access Denied`});
 }
 
-export { errorMiddleware };
+export const handleNotFoundError = (res: Response, message: string) => {
+    res.status(404).json({ message: `The flight you are looking for does not exsist!` });
+}
+
+export const handleUpdateError = (res: Response) => {
+    res.status(500).json({ message: `An error occurred while updating the record` });
+}
+
+export const handlePassengerError = (res: Response, err: any) => {
+    res.status(401).json({ message: `Access Denied`});
+}
+
+export const handlePassengerNotFound = (res: Response, err: any) => {
+    res.status(404).json({ message: `The passenger you are looking for does not exsist!`});
+}
+
+export const handleUpdatePassenger = (res: Response, err: any) => {
+    res.status(500).json({ message: `An error occurred while updating the record`});
+}
+
+export const handleDelayError = (res: Response, err: any) => {
+    res.status(401).json({ message: `Access Denied`});
+}
+
+export const handleDelayNotFound = (res: Response, err: any) => {
+    res.status(404).json({ message: `The passenger you are looking for does not exsist!`});
+}
+
+export const handleUpdateDelay = (res: Response, err: any) => {
+    res.status(500).json({ message: `An error occurred while updating the record`});
+}
